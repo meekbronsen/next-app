3e161d09b1e5e44706969445ee5e059ffd79e60a
meekbronsen <mikolskeezy@gmail.com>
2024-02-03 03:00:36 +0300

    Fetching Data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,27 @@
 // The content below will be rendered when user is on this url -> http://localhost:3000/users
 import React from "react";
 
-const UsersPage = () => {
-  return <div>Users Page</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  // NB: Note that fetch is note inside a useEffect.
+  // Cause we are already on the server
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
