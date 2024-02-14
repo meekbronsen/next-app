c62fd2e9283dcdcc8f6601bafd9d86fd710ffa82
meekbronsen <mikolskeezy@gmail.com>
2024-02-14 13:21:15 +0300

    Config Google Provider.

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,12 +3,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=" p-5 bg-slate-300 flex ">
+    <div className=" p-5 bg-slate-300 flex gap-8">
       <Link href={"/"} className=" mr-5 ">
         Next.js
       </Link>
       <Link href={"/users"} className=" mr-5 ">
         Users
+      </Link>
+      <Link href={"/api/auth/signin"} className=" mr-5 ">
+        Sign In
       </Link>
     </div>
   );
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,14 @@
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google"; // add a provider
 
 // create a handler function
-const handler = NextAuth({});  // call next auth and give it a configuration object
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ]
+});  // call next auth and give it a configuration object
 
 export { handler as GET, handler as POST  } // exporting the handler function from this module
