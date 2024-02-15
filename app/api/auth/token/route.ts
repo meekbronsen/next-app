54f29150f923756ae2bcc89dc1a0c513259453ab
meekbronsen <mikolskeezy@gmail.com>
2024-02-15 12:38:04 +0300

    Understaning sessions and Capturing tokens.

---
+++ app/api/auth/token/route.ts
@@ -0,0 +1,8 @@
+import React from 'react'
+import { getToken } from 'next-auth/jwt'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+    const token = await getToken({ req: request });
+    return NextResponse.json(token);
+}
