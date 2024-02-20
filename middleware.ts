8b94b9cc3920112016cda16000a17fe89874b58a
meekbronsen <mikolskeezy@gmail.com>
2024-02-20 12:38:25 +0300

    Protecting Routes.

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import middleware from "next-auth/middleware";
 
-// We can use this middleware to cock block users
-export function middleware(request: NextRequest){
+export default middleware;
 
-// With this the user will be redirected back to request.url/new-page when he tries to access data without session.
-    return NextResponse.redirect(new URL('/new-page', request.url));
-
-}
-
-// If we want to cock block certain paths.
-// This config object is one of the things Next js is structured to out look for.
 export const config = {
-// This option can be a path
-    // matcher: '' 
-// or an array of paths
-    // matcher:['/users']        
-    // matcher: ['/users/:id*']  // with the wildcard * means zero or more parameters
-    matcher: ['/users/:id+']  // If you use + means one or more
-    // matcher: ['/users/:id?']     // ?: zero or one 
+    matcher: ['/users/:id+']  
 }                            
