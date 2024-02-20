d1cb40969c9a43134bf278cc07208e3bc78eadc0
meekbronsen <mikolskeezy@gmail.com>
2024-02-20 15:35:15 +0300

    Configuring Credentials.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,54 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google"; 
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
 import { NextAuthOptions } from "next-auth";
+import bcrypt from 'bcrypt'
 
+// Implementing log In
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ],
-    session:{
-        strategy: 'jwt'
-    }
-}
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        // label what the forminput will be labled
+        email: { label: "Email", type: "enter your email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "enter your password",
+        },
+      },
 
-const handler = NextAuth(authOptions);  
+      // This function is used for validation of what the user has submitted.
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) return null;
 
-export { handler as GET, handler as POST  } 
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        // If user is not registered
+        if (!user) return null;
+
+        const isAMatch = await bcrypt.compare( credentials.password, user.hashedPassword! )
+
+        return isAMatch ? user : null;
+
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
