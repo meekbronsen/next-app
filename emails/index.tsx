da6a603bec01ec9339b4cf33681c12378cc4e0a2
meekbronsen <mikolskeezy@gmail.com>
2024-02-23 19:19:55 +0300

    Styling react-email components.

--- emails/index.tsx
+++ emails/index.tsx
@@ -1,14 +1,18 @@
-import { Html , Button, Body, Container, Text} from "@react-email/components"
+import { Html , Button, Body, Container, Text, Tailwind} from "@react-email/components"
 import * as React from "react";
 
 export default function Email() {
   return (
     <Html>
-      <Body>
+      {/* For tailwind you have to import the Tailwind component from react-email-components */}
+      <Tailwind>
+      <Body className="bg-pink-500">
         <Container>
-          <Text></Text>
+          {/* For styling you could use inline styles */}
+          <Text style={{fontFamily: 'Inter', fontSize: '2rem'}}>Welcome User</Text>
         </Container>
       </Body>
+      </Tailwind>
       <Button
         href="https://example.com"
         style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
