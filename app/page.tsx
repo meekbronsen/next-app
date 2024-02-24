0c823bbd7a44716a1d23e7822246a512b7ecc2f5
meekbronsen <mikolskeezy@gmail.com>
2024-02-24 21:27:13 +0300

    Optimizing Images. Using the Image component provided by next/image  with various options

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import vacation from "@/public/images/valeriia-bugaiova-_pPHgeHz1uk-unsplash.jpg";
 
 export default async function Home() {
-
-  // this will capture the session
-  const session = await getServerSession( authOptions )
-
   return (
-    <main className="flex flex-col gap-5">
-      <h1 className="font-bold">Hello { session ? <span>{session.user?.name}</span> : 'world' }</h1>
-      <Link href='/users'>User</Link>
-      <ProductCard></ProductCard>
+    // The parent element should have a position defined if youre gonna use Image component
+    <main className="relative h-[200px]">
+      <Image
+        src={"https://bit.ly/react-cover"}
+        alt="image of a hotel pool"
+        fill // if you want the image to be responsive to the space available, its like width set to 100%.
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw" // setting the size of the image, it's like srcset
+        quality={100}  // very important to determine how next js should change the quality of the image
+        priority // a boolean. Images that should be visible right from page load.
+      />
     </main>
   );
 }
