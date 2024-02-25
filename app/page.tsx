8f85bb77b7a986a9c934a9ebee192467526abfd9
meekbronsen <mikolskeezy@gmail.com>
2024-02-25 20:12:12 +0300

    Lazy loading libraries.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 
@@ -6,20 +6,29 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 
 // By moving the import statement of the HeavyComponent into dynamic function.
-const HeavyComponent = dynamic(() => import('./components/HeavyComponent'), 
-{ 
-  loading: () => <p>Loading...</p>, // this option will appear when fetching 
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  loading: () => <p>Loading...</p>, // this option will appear when fetching
   // By default server side rendering performed, so if resources are from third party servers then disable ssr
-  ssr: false 
+  ssr: false,
   // We could also lazy load third party libraries
-})
+});
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(false);
   return (
     <main>
-      <button onClick={() => {setIsVisible(true)}}>Show</button>
-      {isVisible && <HeavyComponent/>}
+      <button
+        onClick={async () => {
+          // Lazy loading lodash lib by importing it during the trigger.
+          // after awaiting the module will be returned and use default to capture the default export
+          const _ = (await import("lodash")).default;
+
+          setIsVisible(true);
+        }}
+      >
+        Show
+      </button>
+      {isVisible && <HeavyComponent />}
     </main>
   );
 }
