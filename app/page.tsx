9fb0cbf7f3e054232306659d89ac5a55309f8fc5
meekbronsen <mikolskeezy@gmail.com>
2024-02-25 15:34:22 +0300

    Using Fonts.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
 import Image from "next/image";
+import { Inter, Roboto } from 'next/font/google';
 import vacation from "@/public/images/valeriia-bugaiova-_pPHgeHz1uk-unsplash.jpg";
+import localFont from 'next/font/local'
+
+// the Inter import returns a class that we use to construct font objects with various configs.
+const inter = Inter({subsets: ['latin']})
+
+const roboto  =  Roboto({
+  subsets: ['latin'], // if we are using a latin language 
+  weight: ['400', '500'],
+
+})
+
+// Applying custom fonts 
+const jetBrains = localFont({
+    src: '../public/fonts/jetbrainsmono-regular-webfont.woff2',
+})
 
 export default async function Home() {
   return (
-    // The parent element should have a position defined if youre gonna use Image component
-    <main className="relative h-[200px]">
-      <Image
-        src={"https://bit.ly/react-cover"}
-        alt="image of a hotel pool"
-        fill // if you want the image to be responsive to the space available, its like width set to 100%.
-        className="object-cover"
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw" // setting the size of the image, it's like srcset
-        quality={100}  // very important to determine how next js should change the quality of the image
-        priority // a boolean. Images that should be visible right from page load.
-      />
+    <main>
+      {/* Applying the font object */}
+      <h1 className={roboto.className}>Hello world</h1>
+      <p className={jetBrains.className}>JetBrains Font</p>
     </main>
   );
 }
