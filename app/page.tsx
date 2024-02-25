490ea3391d696b4587769750478c56a62a66e637
meekbronsen <mikolskeezy@gmail.com>
2024-02-25 20:06:03 +0300

    Lazy loading components and configuring options.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
-import { Metadata } from "next";
+'use client'
 
+import { useState } from "react";
 
+// Use this dynamic function to implement lazy loading
+import dynamic from "next/dynamic";
 
-export default async function Home() {
+// By moving the import statement of the HeavyComponent into dynamic function.
+const HeavyComponent = dynamic(() => import('./components/HeavyComponent'), 
+{ 
+  loading: () => <p>Loading...</p>, // this option will appear when fetching 
+  // By default server side rendering performed, so if resources are from third party servers then disable ssr
+  ssr: false 
+  // We could also lazy load third party libraries
+})
+
+export default function Home() {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <main>
+      <button onClick={() => {setIsVisible(true)}}>Show</button>
+      {isVisible && <HeavyComponent/>}
     </main>
   );
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  const product =  await fetch('');
-  return {
-    title: 'product.title',
-    description: ''
-  }
 }
